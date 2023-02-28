fd587597e8ea3c45530281b46a8f12f0bf36eb6c
debojyoti_biswas <debojyotibiswas00@gmail.com>
2023-03-01 00:23:28 +0530

    this is nwe

---
+++ project2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import About from './Components/About/About';
+import Content from './Components/Content/Content';
+import Home from './Components/Home/Home';
+import 
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+export default App;
+
+
+
